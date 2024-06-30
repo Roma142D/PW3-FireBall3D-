d368e4820c9cf147d706a99b2df30f0f8ae9fe56
Roman <romadoliba@gmail.com>
2024-06-30 13:38:07 +0300

    Initial commit

---
+++ Assets/Scripts/TowerGenerator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RomanDoliba.Tower
+{
+    public class TowerGenerator : MonoBehaviour
+    {
+        [SerializeField] private TowerSectionBase[] _towerSectionsPrefabs;
+        [SerializeField] private Transform _spawnPosition;
+        [SerializeField] private int _sectionsAmount;
+        private List<TowerSectionBase> _towerSections;
+        private Vector3 _currentSpawnPoint;
+        //IsIt?
+
+        private void Awake()
+        {
+            _towerSections = new List<TowerSectionBase>();
+            _currentSpawnPoint = _spawnPosition.position;
+            BuildTower();
+        }
+        private void BuildTower()
+        {
+            for (int i = 0; i < _sectionsAmount; i++)
+            {
+                var spawnedSection = BuildSection();
+                _towerSections.Add(spawnedSection);
+                _currentSpawnPoint = spawnedSection.transform.position;
+            }
+        }
+
+        private TowerSectionBase BuildSection()
+        {
+            var newSection = Instantiate(_towerSectionsPrefabs[Random.Range(0, _towerSectionsPrefabs.Length)],
+            GetNextSpawnPoint(_currentSpawnPoint), Quaternion.identity, this.transform);
+
+            return newSection;
+        }
+        private Vector3 GetNextSpawnPoint(Vector3 spawnedSectionPosition)
+        {
+            var nextSpawnPosition = new Vector3(_spawnPosition.position.x,
+            spawnedSectionPosition.y + _towerSectionsPrefabs[0].transform.localScale.y,
+            _spawnPosition.position.z);
+
+            return nextSpawnPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/TowerSectionBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RomanDoliba.Tower
+{
+    public class TowerSectionBase : MonoBehaviour
+    {
+        
+    }
+}
