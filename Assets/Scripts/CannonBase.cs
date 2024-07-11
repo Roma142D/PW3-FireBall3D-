c9fc1955d1ff133d52b24499e0330fd92213d07f
Roman <romadoliba@gmail.com>
2024-07-11 15:57:49 +0300

    Add shooting interval

--- Assets/Scripts/CannonBase.cs
+++ Assets/Scripts/CannonBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,50 @@ namespace RomanDoliba.Core
 {
     public class CannonBase : MonoBehaviour
     {
-        [SerializeField] private CannonballBase _cannonball;
+        [SerializeField] private Rigidbody _cannonballPrefab;
         [SerializeField] private Transform _target;
         [SerializeField] private Button _shootButton;
         [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _shootInterval;       
+        private Coroutine _shootIntervalRoutine;
         
         private void Awake()
         {
             _shootButton.onClick.AddListener(Shoot);
-            _cannonball._spawnPoint = _spawnPoint;
         }
         
         private void Shoot()
         {
-            _cannonball.Shoot(_target.position);
+            if (_shootIntervalRoutine == null)
+            {
+                ShootCannonball(_target.position);
+            }
         }
+        public void ShootCannonball(Vector3 target)
+        {
+            var cannonballInstance = Instantiate(_cannonballPrefab, _spawnPoint.position, Quaternion.identity);
+            cannonballInstance.AddForce(_speed * target * -1, ForceMode.Impulse);
+
+            _shootIntervalRoutine = StartCoroutine(ShootIntervalRoutine(ClearRoutine));
+        }
+
+        private IEnumerator ShootIntervalRoutine(System.Action callback)
+        {
+            Debug.Log("CoroutineStart");
+            if (_shootButton.isActiveAndEnabled)
+            {
+                Debug.Log("ButtonPresed");
+                yield return new WaitForSeconds(_shootInterval);
+            }
+            callback?.Invoke();
+        }
+
+        private void ClearRoutine()
+        {
+            Debug.Log("ClearRoutine");
+            _shootIntervalRoutine = null;
+        }
+       
     }
 }
