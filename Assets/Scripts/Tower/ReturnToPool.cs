be33c31f13ea49ac0cff0f88281a591e8119b9a4
Roman <romadoliba@gmail.com>
2024-07-19 17:11:26 +0300

    Tried to remake Tower spawn into ObjectPool

---
+++ Assets/Scripts/Tower/ReturnToPool.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace RomanDoliba.Tower
+{
+    [RequireComponent(typeof(TowerSectionBase))]
+    public class ReturnToPool : MonoBehaviour
+    {
+        public TowerSectionBase _towerSection;
+        public IObjectPool<TowerSectionBase> _towerSectionsPool;
+
+        public void Start()
+        {
+            _towerSection = GetComponent<TowerSectionBase>();
+        }
+        public void OnSpawnStopped()
+        {
+            _towerSectionsPool.Release(_towerSection);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerSectionsPooling.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UI;
+
+namespace RomanDoliba.Tower
+{
+    public class TowerSectionsPooling : MonoBehaviour
+    {
+        [SerializeField] private TowerSectionBase _towerSection;
+        [SerializeField] private Transform _spawnPosition;
+        [SerializeField] private int _maxPoolSize;
+        [SerializeField] private Button button;
+        private Vector3 _currentSpawnPoint;
+        private ObjectPool<TowerSectionBase> _towerSectionsPool;
+
+        private void Awake()
+        {
+            _currentSpawnPoint = _spawnPosition.GetPosition();
+            StartCoroutine(BuildTower());
+        }
+        public IObjectPool<TowerSectionBase> TowerSectionsPool
+        {
+            get
+            {
+                if (_towerSectionsPool == null)
+                {
+                    _towerSectionsPool = new ObjectPool<TowerSectionBase>(BuildSection, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, 10, _maxPoolSize);
+                }
+                return _towerSectionsPool;
+            }
+
+        }
+        private IEnumerator BuildTower()
+        {
+            for (int i = 0; i < _maxPoolSize; i ++)
+            {
+                var towerSection = TowerSectionsPool.Get();
+                _currentSpawnPoint = towerSection.GetPosition();
+
+                Debug.Log(_towerSectionsPool.CountAll);
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+        
+        private TowerSectionBase BuildSection()
+        {
+            var newSection = Instantiate(_towerSection, GetNextSpawnPoint(_currentSpawnPoint), Quaternion.identity, this.transform);
+            
+            newSection.gameObject.AddComponent<ReturnToPool>();
+            newSection.GetComponent<ReturnToPool>()._towerSectionsPool = TowerSectionsPool;
+
+            return newSection;
+        }
+        private Vector3 GetNextSpawnPoint(Vector3 spawnedSectionPosition)
+        {
+            var nextSpawnPosition = new Vector3(_spawnPosition.position.x,
+            spawnedSectionPosition.y + _towerSection.transform.localScale.y,
+            _spawnPosition.position.z);
+
+            return nextSpawnPosition;
+        }
+
+        private void OnReturnedToPool(TowerSectionBase towerSection)
+        {
+            towerSection.gameObject.SetActive(false);
+        }
+        private void OnTakeFromPool(TowerSectionBase towerSection)
+        {
+            towerSection.gameObject.SetActive(true);
+        }
+        private void OnDestroyPoolObject(TowerSectionBase towerSection)
+        {
+            Destroy(towerSection.gameObject);
+        }
+    }
+}
